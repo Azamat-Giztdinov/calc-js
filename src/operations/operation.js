8372719124af3351d88d372c819b1255f7c5b58f
azamat <azamat.giztdinov@mail.ru>
2024-07-11 05:19:09 +0300

    update eslint

--- src/operations/operation.js
+++ src/operations/operation.js
@@ -1,4 +1,9 @@
 class Operation {
+    /**
+     * @param {number} a
+     * @param {number} b
+     */
+    // eslint-disable-next-line no-unused-vars
     execute(a, b) {
         throw new Error('Method not implemented');
     }
